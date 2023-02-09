da17ae31d25e2193b79f3971f4c5d35f4b2668c8
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-02-09 19:47:50 +0900

    #34 - 게시판 검색바를 좀 더 예쁘게 하기 위해서 description 변경

--- project-board/src/main/java/com/study/projectboard/domain/type/SearchType.java
+++ project-board/src/main/java/com/study/projectboard/domain/type/SearchType.java
@@ -3,7 +3,7 @@ package com.study.projectboard.domain.type;
 import lombok.Getter;
 
 public enum SearchType {
-    TITLE("제목"), CONTENT("본문"), ID("id"), NICKNAME("닉네임"), HASHTAG("해시태그");
+    TITLE("제목"), CONTENT("본문"), ID("유저 ID"), NICKNAME("닉네임"), HASHTAG("해시태그");
     @Getter private final String description;
 
     SearchType(String description) {
