838b233ef90bae8c173d55030a7df43a3720934a
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-02-26 20:36:36 +0900

    #63 - 해시태그 서비스 기능 구현
    게시글에 달린 해시태그를 관리하기 위한 기능 구현
    해시태그 저장은 게시글이 저장, 수정 될 때만 일어나는 기능으로
    연관관계 매핑을 통해 게시글 서비스에서 모두 처리 가능
    
    * 해시태그 이름으로 해시태그 정보를 db에서 조회하기
    * 본문에서 해시태그 파싱하기
    * 게시글이 더 이상 없는 해시태그 삭제하기

--- project-board/src/main/java/com/study/projectboard/service/HashtagService.java
+++ project-board/src/main/java/com/study/projectboard/service/HashtagService.java
@@ -1,19 +1,47 @@
 package com.study.projectboard.service;
 
+import com.study.projectboard.domain.Hashtag;
+import com.study.projectboard.repository.HashtagRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+@Transactional(readOnly = true)
 @Service
+@RequiredArgsConstructor
 public class HashtagService {
-    public Object parseHashtagNames(String content) {
-        return null;
+
+    private final HashtagRepository hashtagRepository;
+
+    public Set<String> parseHashtagNames(String content) {
+        if (content == null) {
+            return Set.of();
+        }
+        Pattern pattern = Pattern.compile("#[\\w가-힣]+"); // \w -> 알파벳 + 숫자 + _ 중 하나 , 가-힣 -> 한글 전체 , + -> 한개 이상
+        Matcher matcher = pattern.matcher(content.strip()); // strip 문자 앞 뒤의 공백 제거 matcher 를 이용해 정규식 적용
+        Set<String> result = new HashSet<>();
+
+        while (matcher.find()) {
+            result.add(matcher.group().replace("#", "")); // #을 정규식에서 같이 parsing 했었는데 탈락시킨다.
+        }
+
+        return Set.copyOf(result); //  unmodifiable Set 을 만들어서 return 불변성을 만들어 준다.
     }
 
-    public Object findHashtagsByNames(Set<String> expectedHashtagNames) {
-        return null;
+    public Set<Hashtag> findHashtagsByNames(Set<String> hashtagNames) {
+        return new HashSet<>(hashtagRepository.findByHashtagNameIn(hashtagNames)); // List 로 받아서 Set 으로 바꿔 return 해준다.
     }
 
-    public void deleteHashtagWithoutArticles(Object any) {
+    @Transactional
+    public void deleteHashtagWithoutArticles(Long hashtagId) { // 해당 해시태그가 아무런 게시글에도 없을 때 해시태그를 삭제시킨다.
+        Hashtag hashtag = hashtagRepository.getReferenceById(hashtagId);
+        if (hashtag.getArticles().isEmpty()) {
+            hashtagRepository.delete(hashtag);
+        }
     }
 }
