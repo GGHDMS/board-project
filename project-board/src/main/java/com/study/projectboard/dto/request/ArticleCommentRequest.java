d3caaca519054a1ced072dbdb16562d5b2b5289d
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-03-01 17:31:39 +0900

    #84 - 대댓글 비즈니스 로직 구현
    부모/ 자식 관계를 만들면서 enity < - > dto 간 변환 로직,
    댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법,
    댓글과 대댓글을 db 데이터로부터 변환 할 때
    서로 계층 구조를 만들고 각각 정렬하는 과정을 구현

--- project-board/src/main/java/com/study/projectboard/dto/request/ArticleCommentRequest.java
+++ project-board/src/main/java/com/study/projectboard/dto/request/ArticleCommentRequest.java
@@ -8,21 +8,28 @@ import lombok.Data;
 @Data
 public class ArticleCommentRequest {
     Long articleId;
+    Long parentCommentId;
     String content;
 
-    private ArticleCommentRequest(Long articleId, String content) {
+    private ArticleCommentRequest(Long articleId, Long parentCommentId, String content) {
         this.articleId = articleId;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
-    public static ArticleCommentRequest of(Long articleId, String content){
-        return new ArticleCommentRequest(articleId, content);
+    public static ArticleCommentRequest of(Long articleId, String content){ // 부모 댓글
+        return ArticleCommentRequest.of(articleId, null,  content);
+    }
+
+    public static ArticleCommentRequest of(Long articleId, Long parentCommentId, String content){ //대댓글
+        return new ArticleCommentRequest(articleId, parentCommentId, content);
     }
 
     public ArticleCommentDto toDto(UserAccountDto userAccountDto){
         return ArticleCommentDto.of(
                 articleId,
                 userAccountDto,
+                parentCommentId,
                 content
         );
     }
--- project-board/src/main/java/com/study/projectboard/dto/response/ArticleCommentResponse.java
+++ project-board/src/main/java/com/study/projectboard/dto/response/ArticleCommentResponse.java
@@ -4,28 +4,43 @@ import com.study.projectboard.dto.ArticleCommentDto;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 
 @Data
-public class ArticleCommentResponse {
+public class ArticleCommentResponse { // 댓글에 대한 정보
     Long id;
     String content;
     LocalDateTime createdAt;
     String email;
     String nickname;
     String userId;
+    Long parentCommentId;
+    Set<ArticleCommentResponse> childComments;
 
-    public ArticleCommentResponse(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
+
+    public ArticleCommentResponse(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId, Long parentCommentId, Set<ArticleCommentResponse> childComments) {
         this.id = id;
         this.content = content;
         this.createdAt = createdAt;
         this.email = email;
         this.nickname = nickname;
         this.userId = userId;
+        this.parentCommentId = parentCommentId;
+        this.childComments = childComments;
     }
 
     public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
-        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId);
+        return ArticleCommentResponse.of(id, content, createdAt, email, nickname, userId, null);
+    }
+
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId, Long parentCommentId) {
+        Comparator<ArticleCommentResponse> childCommentComparator = Comparator // childComments 에 정렬된 set 이 들어가야 되기 때문에 사용했다. set 의 규칙을 정할 수 있다.
+                .comparing(ArticleCommentResponse::getCreatedAt) // 기본 오른차순, 내림하고 싶으면 .reversed
+                .thenComparing(ArticleCommentResponse::getId);
+        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId, parentCommentId, new TreeSet<>(childCommentComparator));
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto) {
@@ -34,13 +49,18 @@ public class ArticleCommentResponse {
             nickname = dto.getUserAccountDto().getUserId();
         }
 
-        return new ArticleCommentResponse(
+        return ArticleCommentResponse.of( //Dto 로 부터 생성
                 dto.getId(),
                 dto.getContent(),
                 dto.getCreatedAt(),
                 dto.getUserAccountDto().getEmail(),
                 nickname,
-                dto.getUserAccountDto().getUserId()
+                dto.getUserAccountDto().getUserId(),
+                dto.getParentCommentId()
         );
+    }
+
+    public boolean hasParentComment() {
+        return getParentCommentId() != null;
     }
 }
