3ec75d4e21b0269e679c18e92bec8cdec95f7bb7
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-09-05 19:12:13 +0900

    #105 - refactor: api 호출 오류 해결
    레포지토리 에 @RepositoryRestResource 가 생략되어 있어
    api 호출시 에러가 발생했다
    그래서 추가함

--- project-board/src/main/java/com/study/projectboard/repository/UserAccountRepository.java
+++ project-board/src/main/java/com/study/projectboard/repository/UserAccountRepository.java
@@ -2,6 +2,8 @@ package com.study.projectboard.repository;
 
 import com.study.projectboard.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
