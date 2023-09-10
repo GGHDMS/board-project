d159c95cdbf5e8b025b433950dcd213c166a141f
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-09-10 18:35:40 +0900

    #109 - UserAccount api 노출 추가 + data rest projection 을 이용한 데이터 노출 필드 설정
    `UserAccount` 를 노출할 api 로 추가해줌
    이렇게 하면 다른 게시글, 댓글 등의 data rest api 노출 데이터 형식이
    원하지 않는 않는 스타일로 바뀌게 된다.
    이를 수동으로 설정하기 위해 projection 기능을 사용하여
    노출할 필들들을 직접 지정함.

---
+++ project-board/src/main/java/com/study/projectboard/domain/projection/ArticleCommentProjection.java
@@ -0,0 +1,26 @@
+package com.study.projectboard.domain.projection;
+
+import com.study.projectboard.domain.ArticleComment;
+import com.study.projectboard.domain.UserAccount;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.time.LocalDateTime;
+
+@Projection(name = "withUserAccount", types = ArticleComment.class)
+public interface ArticleCommentProjection {
+    Long getId();
+
+    UserAccount getUserAccount();
+
+    Long getParentCommentId();
+
+    String getContent();
+
+    LocalDateTime getCreatedAt();
+
+    String getCreatedBy();
+
+    LocalDateTime getModifiedAt();
+
+    String getModifiedBy();
+}
--- project-board/src/main/java/com/study/projectboard/repository/UserAccountRepository.java
+++ project-board/src/main/java/com/study/projectboard/repository/UserAccountRepository.java
@@ -1,9 +1,10 @@
 package com.study.projectboard.repository;
 
 import com.study.projectboard.domain.UserAccount;
+import com.study.projectboard.domain.projection.UserAccountProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = UserAccountProjection.class)
 public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
